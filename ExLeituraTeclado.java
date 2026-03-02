385ebef769891fd66bc9501a792fbb4d50588770
marco <marco.antoni910@gmail.com>
2026-03-02 11:20:23 -0300

    ex. declaração de variaveis, leitura e saida de dados

--- ExLeituraTeclado.java
+++ ExLeituraTeclado.java
@@ -1,5 +1,8 @@
 // Código para fazer declaração de variaveis e leitura do teclado
 
+// para fazer um input, é necessário a linha abaixo
+import java.util.Scanner;
+
 class ExLeituraTeclado {
 	
 	/* Essa é a função principal do programa em Java
@@ -9,10 +12,29 @@ class ExLeituraTeclado {
 	 * O caractere "*" seguido por "/" encerra o comentário
 	 */
 	public static void main (String args[]) {
+		
+		// criando a variavel auxiliar para poder ler do teclado
+		// essa variavel será do tipo Scanner e ela permitirá acessar a função
+		// que permitirá ler os dados do teclado
+		// a variavel terá o nome de "sc", pois é uma abreviação de "Scanner"
+		
+		Scanner sc = new Scanner(System.in); 
+		
+		// print para mostrar uma mensagem 
+		System.out.printf("Diga seu nome \n");
+		String nome = sc.nextLine();	// lendo uma string do teclado
+		
+		System.out.printf("Diga sua idade \n");
+		int idade = sc.nextInt();	// lendo um int do teclado
+		
+		System.out.printf("Diga sua altura");
+		float altura = sc.nextFloat();
+		
+		
 		// declarando variaiveis
-		String nome = "Augusto";	// variavel do tipo string
-		int idade = 16;				// variavel do tipo int
-		float altura = 1.76f;		// variavel do tipo float
+		//String nome = "Augusto";	// variavel do tipo string
+		//int idade = 16;				// variavel do tipo int
+		//float altura = 1.76f;		// variavel do tipo float
 		boolean doadorDeOrgaos = true;	// variavel do tipo boolean. true indica que é doador de órgãos e false que não é
 
 		// imprimindo os dados na tela
