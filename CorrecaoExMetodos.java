f0c1f758bead0e86b593c8c7b9522c455709fd23
Marco Antoni <marco.antoni910@gmail.com>
2026-06-22 10:34:48 -0300

    Correção exercícios métodos

--- CorrecaoExMetodos.java
+++ CorrecaoExMetodos.java
@@ -15,6 +15,27 @@ class CorrecaoExMetodos {
 		// para quilômetros com duas casas decimais.
 		System.out.printf("%d metros equivale a %.2f KM \n", metros, distanciaEmKm);
 	}
+	
+	// Exercicio : Crie um método chamada toFahrenheit, que recebe uma temperatura em graus Celsius, 
+	// transforma em Fahrenheit e retorna esse dado. Utilize a fórmula: (°C * 9/5) + 32
+	
+	public float toFahrenheit(float celsius) {
+		
+		return (celsius * 9/5) + 32;
+		
+	}
+	
+	// Exercicio 3: Crie um método chamado calcularMedia, que recebe dois 
+	// parâmetros (uma distância percorrida em km por um veículo) e a quantidade 
+	// consumida de combustível, ambos do tipo double. O consumo é calculado 
+	// dividindo a distância percorrida pela quantidade de combustível. 
+	// O método deve retornar esse valor.
+	// a média representa quantos km o veiculo faz com 1 litro de combustível
+	public double calcularMedia(double distancia, double consumo) {
+		
+		return distancia / consumo;
+	}
+	
 
 	// Método principal do programa.
 	// É o ponto de entrada da aplicação e será utilizado para testar o método criado.
@@ -26,25 +47,25 @@ class CorrecaoExMetodos {
 		
 		// Chama o método passando o valor 8870 metros como argumento.
 		ex.metrosToKM(8870);
+		
+		// no dia 22 de junho às 10:18 a temperatura está 	11,9º
+		// testando o método toFahrenheit 
+		
+		float temperaturaConvertida = ex.toFahrenheit(11.9f);
+		System.out.printf("%.2f graus celsius equivalem a %.2f graus fahrenheit \n", 
+			11.9, temperaturaConvertida); 
+			
+		
+		// testando a correção do exercício 3
+		// criando variaveis auxiliares para mostrar os valores atualizados dentro do print
+		double distancia = 100;
+		double consumo = 11;
+		
+		// o método calcularMedia está sendo chamado diretamente dentro do print
+		System.out.printf("O carro percorreu %.2f km com %.2f litros de combustivel e seu consumo foi %.2f \n",
+			distancia, consumo, ex.calcularMedia(distancia, consumo) );
+			
+		
 	}
 }
 
-class CorrecaoExMetodos {
- 
-
-	public void metrosToKM(int metros) {
-		float distanciaEmKm = metros / 1000f;
-		
-		System.out.printf("%d metros equivale a %.2f KM \n", metros, distanciaEmKm);
-	}
-
-	// criando o método main para testar
-	public static void main (String args[]) {
-		
-		CorrecaoExMetodos ex = new CorrecaoExMetodos();
-		
-		// chamando o método metrosToKM
-		ex.metrosToKM(8870); 
-		
-	}
-}
